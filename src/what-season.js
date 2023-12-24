ae374f43b12dfec325f00a41d75bc8a12b479301
JulKorzh <julkorzh1@gmail.com>
2023-12-24 21:04:30 +0300

    feat: what seasons

--- src/what-season.js
+++ src/what-season.js
@@ -11,15 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/*date = 'Error'*/) {
-  throw new NotImplementedError('Not implemented');
-  //if(arguments === 'Error') return 'Unable to determine the time of year!'
-  //if (typeof date != 'object') return 'Invalid date!'
-  //if (date.getMonth() < 2) return 'winter';
-  //if (date.getMonth() < 5 ) return 'spring';
- // if (date.getMonth() < 8) return 'summer';
- // if (date.getMonth() < 11) return 'autumn';
-//return 'winter';
+function getSeason(date = 'Error') {
+  //throw new NotImplementedError('Not implemented');
+  if (arguments === 'Error') return 'Unable to determine the time of year!';
+  function isValidDate(date) {
+    if (!(date instanceof Date)) {
+      return false
+    }
+    return !isNaN(date.getTime())
+  }
+  if(!isValidDate(date)) throw new Error('Invalid date!')
+  if (isValidDate(date) ) {
+    if (date.getMonth() < 2) return 'winter';
+    if (date.getMonth() < 5) return 'spring';
+    if (date.getMonth() < 8) return 'summer';
+    if (date.getMonth() < 11) return 'autumn';
+    return 'winter';
+  }
+  
+  throw new Error('Invalid date!')
+
 }
 
 module.exports = {
